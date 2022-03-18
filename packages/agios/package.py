6a76d4696ee74ebd9e2fbd35342932cf73ba0e85
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2022-03-18 11:45:20 +0100

    AGIOS: remove FIXME

--- packages/agios/package.py
+++ packages/agios/package.py
@@ -3,30 +3,12 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install agios
-#
-# You can edit this file again by typing:
-#
-#     spack edit agios
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Agios(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """an I/O request scheduling library at file level"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/francielizanon/agios"
     url      = "https://github.com/francielizanon/agios"
     git      = "https://github.com/francielizanon/agios.git"
